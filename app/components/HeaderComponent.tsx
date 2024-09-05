e1eb8dcc8145f48797705ae6aa159670d5642b70
Alex Lévy <alexlevy0@gmail.com>
2024-09-06 01:03:42 +0200

    feat(header): add button interaction to HeaderComponent
    Refactored `HeaderComponent` to include buttons with onPress event handlers for improved user interaction. Updated the component to accept an `onPressFaceLivenessDetector` callback prop. Enhanced UI with buttons for more intuitive user prompts: "Jouer", "Jouer?", and "Jouer!"

--- app/components/HeaderComponent.tsx
+++ app/components/HeaderComponent.tsx
@@ -4,22 +4,41 @@
  */
 
 import type React from "react";
-import { XStack, Text } from "tamagui";
+import { XStack, Text, Button } from "tamagui";
 
 /**
  * Header component
  * @returns {React.ReactElement} The rendered header with vertically centered text
  */
-export const Header: React.FC = () => {
+export const Header: React.FC<{ onPressFaceLivenessDetector: () => void }> = ({ onPressFaceLivenessDetector }) => {
+	const onPress = () => onPressFaceLivenessDetector();
 	return (
-		<XStack flex={1}  jc="space-between" ai="center" px="$4" py="$2">
+		<XStack flex={1} jc="space-between" ai="center" px="$4" py="$2">
 			<XStack ai="center">
 				<Text>MeloCap</Text>
 			</XStack>
 			<XStack space="$4" ai="center">
-				<Text>Jouez</Text>
-				<Text>Jouez?</Text>
-				<Text>Jouer!</Text>
+				<Button
+					onPress={onPress}
+					size="$3"
+					variant="outlined"
+				>
+					Jouer
+				</Button>
+				<Button
+					onPress={onPress}
+					size="$3"
+					variant="outlined"
+				>
+					Jouer?
+				</Button>
+				<Button
+					onPress={onPress}
+					size="$3"
+					variant="outlined"
+				>
+					Jouer!
+				</Button>
 			</XStack>
 		</XStack>
 	);
