0405e6d3b50cdb8a9c90108e3109c6da47c7943f
Alex Lévy <alexlevy0@gmail.com>
2024-09-09 01:40:44 +0200

    feat(Babel): Add Tamagui plugin to Babel config for enhanced UI
    Updated the Babel configuration to include the Tamagui plugin, enabling advanced UI component functionalities and configurations such as component-specific settings, log timings, and conditional extraction based on environment. Preserved existing Reanimated plugin for animations compatibility

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["react-native-reanimated/plugin"],
-  };
-};
+module.exports = (api) => {
+	api.cache(true)
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"@tamagui/babel-plugin",
+				{
+					components: ["tamagui"],
+					config: "./tamagui.config.ts",
+					logTimings: true,
+					disableExtraction: process.env.NODE_ENV === "development",
+				},
+			],
+			"react-native-reanimated/plugin",
+		],
+	}
+}
