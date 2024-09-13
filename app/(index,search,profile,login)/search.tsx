95281e7401298921a850097a93fc51de2a8ee8f3
Alex Lévy <alexlevy0@gmail.com>
2024-09-13 22:58:33 +0200

    feat(spotify): Integrate Spotify search functionality in search component
    Added functionality to search Spotify within the search component.
    Introduced state management and useEffect to handle API calls to
    fetch search results. This enhances the search feature by
    providing Spotify content, improving user experience and application
    capabilities.
    
    Console log statements added and old log commented out for debugging

--- app/(index,search,profile,login)/search.tsx
+++ app/(index,search,profile,login)/search.tsx
@@ -1,18 +1,20 @@
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import { Button } from "@tamagui/button";
+import { MoreVertical } from "@tamagui/lucide-icons";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 
 import { Feed } from "@/components/feed";
 import { posts } from "@/data";
 import { goldenRatio } from "@/utils";
 import { onPressBottomSheet } from "@/utils/bottomSheet";
-import { MoreVertical } from "@tamagui/lucide-icons";
-import { useAuthenticator } from "@aws-amplify/ui-react-native";
-import { useActionSheet } from "@expo/react-native-action-sheet";
-import { Button } from "@tamagui/button";
+import { getSpotifyAccessToken, searchSpotify } from "@/utils/spotify/getSpotifyToken";
 
 export default function Search() {
 	const router = useRouter();
 	const params = useLocalSearchParams<{ q?: string }>();
-	console.log({ posts });
+	// console.log({ posts });
 	const filteredPosts = !params.q
 		? posts
 		: posts.filter((post) =>
@@ -21,6 +23,7 @@ export default function Search() {
 				.includes(params.q.toLowerCase()),
 		);
 
+	console.log({ filteredPosts });
 
 	const { authStatus } = useAuthenticator((context) => [
 		context.authStatus,
@@ -31,6 +34,20 @@ export default function Search() {
 	const onPress = () => {
 		onPressBottomSheet({ signOut, showActionSheetWithOptions, isLoggedIn, authStatus });
 	};
+	const { searchResult, setSearchResult} = useState()
+
+	useEffect(() => {
+		const fetch = async () => {
+			const accessToken = await getSpotifyAccessToken();
+			console.log({ accessToken });
+			const data = await searchSpotify({ query: params.q ?? "", accessToken })
+			
+			setSearchResult(data)
+		}
+
+		// fetch()
+	}, [])
+
 	return (
 		<>
 			<Stack.Screen
