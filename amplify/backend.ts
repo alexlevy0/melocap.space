1837dfcaf82d14d1bd55798c9ee7ada7c372e8b6
Alex Lévy <alexlevy0@gmail.com>
2024-09-15 09:42:28 +0200

    feat(function): add function to Amplify backend
    Integrated a new `sayHello` function resource into the Amplify backend configuration, enhancing our app's functionality. Adjusted import statements for consistency and readability. Retained and reformatted policy attachments to ensure both authenticated and guest users can access the Rekognition service as defined

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,26 +1,28 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
 import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { sayHello } from './functions/say-hello/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  auth,
-  data,
-});
+	auth,
+	data,
+	sayHello,
+})
 
-const livenessStack = backend.createStack("liveness-stack");
+const livenessStack = backend.createStack("liveness-stack")
 
 const livenessPolicy = new Policy(livenessStack, "LivenessPolicy", {
-  statements: [
-    new PolicyStatement({
-      actions: ["rekognition:StartFaceLivenessSession"],
-      resources: ["*"],
-    }),
-  ],
-});
-backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows guest user access
-backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows logged in user access
+	statements: [
+		new PolicyStatement({
+			actions: ["rekognition:StartFaceLivenessSession"],
+			resources: ["*"],
+		}),
+	],
+})
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(livenessPolicy) // allows guest user access
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(livenessPolicy) // allows logged in user access
