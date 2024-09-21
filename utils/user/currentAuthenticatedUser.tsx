2231c25773631094fd160fa382cb183f784e470e
Alex Lévy <alexlevy0@gmail.com>
2024-09-21 12:36:54 +0200

    feat(user attr): Enhance user fetching functionality with attribute updates
    Added functionality to fetch user attributes in addition to the current user data using `fetchUserAttributes`. Introduced a placeholder for updating a user attribute, aiming for future integration with more dynamic attribute management. Improved logging for better debugging and tracking of fetched data

--- utils/user/currentAuthenticatedUser.tsx
+++ utils/user/currentAuthenticatedUser.tsx
@@ -1,10 +1,14 @@
-import { getCurrentUser } from "aws-amplify/auth";
+import { getCurrentUser, fetchUserAttributes } from "aws-amplify/auth";
+import { handleUpdateUserAttribute } from "./updateUserAttribute";
 
 export async function currentAuthenticatedUser() {
         try {
-                // @ts-expect-error loginId exist only in signInDetails
-                const { username, userId, signInDetails: { loginId } } = await getCurrentUser();
-                return { username, userId, loginId };
+                const currentUser = await getCurrentUser();
+                const userAttributes = await fetchUserAttributes();
+                console.log({ currentUser, userAttributes });
+                // TODO Move this call updateUserAttribute, for example
+                handleUpdateUserAttribute('custom:reward', "0")
+                return currentUser;
         } catch (err) {
                 console.log(err);
         }
